bc19e04bf67474f954ec7f7970d4e44a4160cd98
tianhao <1742166218@qq.com>
2018-06-03 22:14:24 +0800

    wq
    ommit
    aagtqtgqtg Please enter the commit message for your changes. Lines starting
    
    help
    
    "helper
    "
    
    commit -m "first
    
    zaad,afo
    
       a,gofqg".
    
    	#agqnoqgiu'
    
    	'
    
    	omqfggbnqoitg

--- SQLConnectionTest/Program.cs
+++ SQLConnectionTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +13,29 @@ namespace SQLConnectionTest
 	{
 		static void Main(string[] args)
 		{
-			string connStr = ""
-			using (SqlConnection conn = new SqlConnection())
+			////文件流读写
+			//using (Stream inStream = new FileStream(@"1.txt", FileMode.OpenOrCreate))
+			//using (StreamWriter writer = new StreamWriter(inStream,Encoding.ASCII))
+			//{
+			//	writer.WriteAsync("I'm a Student.");
+			//}
+			//using (Stream inStream = new FileStream("1.txt", FileMode.Open))
+			//using (StreamReader reader = new StreamReader(inStream,Encoding.ASCII))
+			//{
+			//	string text = reader.ReadToEnd();
+			//	Console.WriteLine(text);
+			//}
+			//Console.ReadKey();
+
+			Action a = new Action(() => Console.WriteLine("Action委托"));
+			a += () => Console.WriteLine("qgatq");
+			Delegate[] list = a.GetInvocationList();
+			foreach(var item in list)
+			{
+				Console.WriteLine(item.GetType());
+			}
+			a();
+			Console.ReadKey();
 		}
 	}
 }
