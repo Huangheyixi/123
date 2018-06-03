fa7f6db0bd507a80c2a4496f68e8340bfe700a50
tianhao <1742166218@qq.com>
2018-06-03 23:04:51 +0800

    init3

---
+++ MvcAndEasyUI/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using MvcAndEasyUI.TestObj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAndEasyUI.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public ActionResult Show()
+		{
+			List<Person> list = new List<Person>();
+			list.Add(new Person() { ID = 1, Name = "Json1" });
+			list.Add(new Person() { ID = 1, Name = "Json2" });
+			list.Add(new Person() { ID = 2, Name = "Json3" });
+
+			List<dynamic> resultList = new List<dynamic>();
+
+			foreach (var p in list.GroupBy(p => p.ID))
+			{
+				List<dynamic> data=new List<dynamic>();
+				foreach (var item in p)
+				{
+					data.Add(new { Name = item.Name });
+				}
+				resultList.Add(new { ID = p.Key, data = data });
+			}
+
+
+			return Json(new { state="OK",data=list});
+		}
+    }
+
+	
+}
---
+++ WebMVCApplication1/Controllers/TestStringController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace WebMVCApplication1.Controllers
+{
+    public class TestStringController : Controller
+    {
+        // GET: TestString
+        public ActionResult Index()
+        {
+			int intVal = "1".AsInt();
+			DateTime time = "2018/1/10".AsDateTime();
+			decimal d1 = "1.1".As<decimal>();
+            return View();
+        }
+    }
+}
